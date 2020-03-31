84a5d3edb4c38935754e64715b944ae714a6f25c
ctoren <62685988+ctoren@users.noreply.github.com>
2020-03-30 18:04:21 -0700

    Update app.js

--- module7-solution/app.js
+++ module7-solution/app.js
@@ -1,65 +1,93 @@
-(function () {
-'use strict';
+(function ()){
+  "use strict";
 
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+.filter('angularDollars', dollarsFilter);
 
-ToBuyController.$inject = ['ShoppingListCheckOffService'];
+ToBuyController.$inject = ['ShoppingListCheckOffService', 'dollarsFilter'];
+
 function ToBuyController(ShoppingListCheckOffService) {
-  var toBuyController = this;
+  var list1 = this;
 
-  //itemToBuy.itemName = "";
-  //itemToBuy.itemQuantity = "";
+  list1.buys = ShoppingListCheckOffService.getItems();
 
-  //itemToBuy.addItem = function () {
-  //ShoppingListCheckOffService.addItem(itemToBuy.itemName, itemToBuy.itemQuantity);
-  //}
-}
-
-
-AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-function AlreadyBoughtController(ShoppingListCheckOffService) {
-  var alreadyBoughtController = this;
-
-  alreadyBoughtController.items = ShoppingListCheckOffService.getItems();
-
-  //alreadyBought.removeItem = function (itemIndex) {
-    //ShoppingListCheckOffService.removeItem(itemIndex);
-  };
-//}
-
-
-function ShoppingListCheckOffService() {
-  var service = this;
-
-  // List of shopping items
-  var items = [{name: "milk", quantity: 2}, {name: "eggs", quantity: 3}, {name: "bread", quantity: 3}, {name: "cookies", quantity: 10}, {name: "bananas", quantity: 5}];
-
-  var alreadyBought = [];
-
-  service.addItem = function (name, quantity) {
-    var item = {
-      name: name,
-      quantity: quantity
-    };
-    items.push(item);
+  list1.removeItem = function(itemIndex) {
+    var bought = ShoppingListCheckOffService.buyItem(itemIndex);
+    ShoppingListCheckOffService.boughtItem(bought.name, bought.quantity, bought.price);
   };
 
-  service.removeItem = function (itemIndex) {
-    items.splice(itemIndex, 1);
-    //push to alreadyBought
-    alreadyBought.push(item);
-  };
-
-  service.getItems = function () {
-    return items;
-  };
-  service.getAlreadyBought = function() {
-    return alreadyBought;
+  list1.getPrice = function(itemIndex) {
+    var bought = ShoppingListCheckOffService.getItem(itemIndex,"groceries");
   };
   }
 
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
-})();
+  function AlreadyBoughtController(ShoppingListCheckOffService){
+    var list2 = this;
+    list2.buys = ShoppingListCheckOffService.getNewItems();
+
+    list2.getPrice = function(itemIndex) {
+      var bought = ShoppingListCheckOffService.buyItem(itemIndex, "groceries");
+      return ShoppingListCheckOffService.getPrice(bought.price, bought.quantity);
+    };
+  }
+
+  ShoppingListCheckOffService.$inject = ['dollarsFilter'];
+
+  function dollarsFilter() {
+    return function (price, quant) {
+      while(price =! 0 && quant =! 0){
+        var total = price * quant;
+        return '$$$' + total.toFixed(2);
+      }
+    };
+  }
+
+function ShoppingListCheckOffService(dollarsFilter){
+  var service = this,
+      groceries = { isEmpty: false, items: [{name: "eggs", quantity: 3, price: 3}, {name: "bread", quantity: 2, price: 3.50}, {name: "milk", quantity: 1, price: 2}, {name: "cookies", quantity: 10, price: 1}, {name: "chicken", quantity: 2, price, 6.50}]}, list2 = { items: [], isEmpty: true};
+
+      service.boughtItem = function(name, quantity, price) {
+        var item = { name: name, quantity: quantity, price: price};
+        list2.items.push(item);
+        if(list2.items.length > 0){
+          list2.isEmpty = false;
+        }
+      };
+
+     service.buyItem = function(itemIndex) {
+      var item = groceries.items[itemIndex];
+      groceries.items.splice(itemIndex, 1);
+      if(groceries.items.length = 0) {
+        groceries.isEmpty = true;
+      }
+      return item;
+    };
+
+    service.getItem = function(itemIndex, buys){
+      var item;
+      if(buys === "groceries") {
+        item = groceries.items[itemIndex];
+      } else {
+        item = list2.items[itemIndex];
+      }
+      return item;
+    }
+
+    service.getPrice = function(price, quant) {
+      return dollarsFilter(price, quant);
+    };
+
+    service.getItems = function() {
+      return groceries;
+    };
+
+    service.getNewItems = function() {
+      reutrn list2;
+    };
+}
+}());
