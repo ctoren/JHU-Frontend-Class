f7c19162db90fe94d6e9c8600160b50a70273e3c
ctoren <62685988+ctoren@users.noreply.github.com>
2020-04-27 14:38:25 -0700

    Update categories.component.js

--- module9-solution/src/categories.component.js
+++ module9-solution/src/categories.component.js
@@ -4,17 +4,18 @@
 angular.module('MenuApp')
 .controller('CategoriesController', CategoriesController)
 .component('categories', {
-  template 'templates/categories.template.html',
-  bindings: {
-  categories: '<'
+  template: 'templates/categories.template.html',
+   bindings: {
+    categories: '<'
   }
-  });
-  
-  
-  CategoriesController.$inject = ['categories'];
-  function CategoriesController(categories) {
+});
+
+
+CategoriesController.$inject = ['categories'];
+function CategoriesController(categories) {
   var $ctrl = this;
   $ctrl.categories = categories;
-  }
-  
-  })();
+}
+
+
+})();
