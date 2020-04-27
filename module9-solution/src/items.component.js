da8231a05fd64b63baf97cdd6f8e0a86f42f91c7
ctoren <62685988+ctoren@users.noreply.github.com>
2020-04-27 14:37:36 -0700

    Update items.component.js

--- module9-solution/src/items.component.js
+++ module9-solution/src/items.component.js
@@ -4,16 +4,9 @@
 angular.module('MenuApp')
 .controller('ItemsController', ItemsController)
 .component('categories', {
-    template: 'templates/items.template.html',
-     bindings: {
-     items: '<'
-    }
-    });
-    
-    ItemsController.$inject = ['items'];
-    function ItemsController (items) {
-    var $itemsCtrl = this;
-    $itemsCtrl.items = items;
-    
-    }
-    })();
+  template: 'templates/items.template.html',
+   bindings: {
+    items: '<'
+  }
+});
+
